b63812290f103a41aaba001c4d3ad3feab1d6f39
mossa-Sammer <mossa708496@gmail.com>
2020-01-12 15:07:44 +0200

    made signup route tests

---
+++ server/__tests__/api/signup.js
@@ -0,0 +1,97 @@
+const supertest = require('supertest');
+const initDatabase = require('../../db/config/build');
+const connection = require('../../db/config/connection');
+const app = require('../../app');
+
+const { signup } = require('../../controllers');
+
+describe('test the signup route', () => {
+	beforeAll(() => {
+		return initDatabase();
+	});
+
+	afterAll(() => {
+		return connection.end();
+	});
+
+	test('test the signup route with valid data', done => {
+		// expect.assertions(2);
+		const userData = {
+			firstName: 'mossa',
+			lastName: 'dbabesh',
+			email: 'mossa1@gmail.com',
+			age: 20,
+			password: 'mossa123',
+			confirmPassword: 'mossa123',
+		};
+		supertest(app)
+			.post('/api/v1/signup')
+			.send(userData)
+			.expect(200)
+			.expect('Content-Type', /json/)
+			.end((err, res) => {
+				if (err) return done(err);
+				return done();
+			});
+	});
+	test('test with invalid data', done => {
+		const userData = {
+			firstName: 'mos',
+			lastName: 'dbabesh',
+			email: 'mossa1@gmail.com',
+			age: 5,
+			password: 'mossa123',
+			confirmPassword: 'mossa123',
+		};
+		supertest(app)
+			.post('/api/v1/signup')
+			.send(userData)
+			.expect(400)
+			.expect('Content-Type', /json/)
+			.end((err, res) => {
+				if (err) return done(err);
+				done();
+			});
+	});
+	test('test with unneccssary data in the body', done => {
+		const userData = {
+			firstName: 'mos',
+			lastName: 'dbabesh',
+			email: 'mossa1@gmail.com',
+			age: 5,
+			password: 'mossa123',
+			confirmPassword: 'mossa123',
+			fakething: 'dsfsdfsd',
+		};
+		supertest(app)
+			.post('/api/v1/signup')
+			.send(userData)
+			.expect(400)
+			.expect('Content-Type', /json/)
+			.end((err, res) => {
+				if (err) return done(err);
+				done();
+			});
+	});
+
+	test('test with duplicated email', done => {
+		const userData = {
+			firstName: 'mossa',
+			lastName: 'dbabesh',
+			email: 'mossa@gmail.com',
+			age: 20,
+			password: 'mossa123',
+			confirmPassword: 'mossa123',
+		};
+		supertest(app)
+			.post('/api/v1/signup')
+			.send(userData)
+			.expect(400)
+			.expect('Content-Type', /json/)
+			.end((err, res) => {
+				if (err) return done(err);
+				console.log(res.body);
+				done();
+			});
+	});
+});
